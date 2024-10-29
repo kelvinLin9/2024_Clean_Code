69c6b4acf711e997217f2d5face9f8c77af21f0c
kelvin <kelvin80121@gmail.com>
2024-10-29 19:10:52 +0800

    Refactor variable names and remove commented out code

--- ch1/1-2.js
+++ ch1/1-2.js
@@ -1,42 +1,25 @@
-// function updateTodoList(todoList){
+// function manageTodoList(todoList) {
+//   // 更新待辦事項清單
 //   todoList.forEach((todo, index) => {
 //     if (!todo.completed) {
 //       todo.completed = true;
 //     }
 //   });
-// }
-// function notifyUser() {
+//   // 通知用戶
 //   console.log('已完成的待辦事項清單：', todoList);
 // }
 
-// function manageTodoList(todoList) {
-//   // 更新待辦事項清單
-//   updateTodoList(todoList)
-//   // 通知用戶
-//   notifyUser()
-// }
+
+
 
 // function printInfo(person) {
 //   console.log(`Name: ${person.name}`);
 //   console.log(`Age: ${person.age}`);
 // }
 
-function printStudentInfo(student) {
-  console.log(`Name: ${student.name}`);
-  console.log(`Age: ${student.age}`);
-  console.log(`Major: ${student.major}`);
-}
-
-function printBasicInfo (person) {
-  console.log(`Name: ${person.name}`);
-  console.log(`Age: ${person.age}`);
-}
-
-// function printInfo (person) {
-//   printBasicInfo(person)
+// function printStudentInfo(student) {
+//   console.log(`Name: ${student.name}`);
+//   console.log(`Age: ${student.age}`);
+//   console.log(`Major: ${student.major}`);
 // }
 
-function printStudentInf (person) {
-  printBasicInfo(person)
-  console.log(`Major: ${student.major}`);
-}
--- ch1/1-3 copy 2.js
+++ ch1/1-3 copy 2.js
@@ -26,59 +26,46 @@ const bmiStatesData = {
   },
 }
 
-function calculateBMI(weight, height) {
-  return (weight / ((height / 100) * (height / 100))).toFixed(2);
-}
-function getBmiState (bmi) {
-  let bmiState = ''
-  if (bmi < 18.5) {
+
+function bmi(height, weight) {
+  const x = (weight / ((height / 100) * (height / 100))).toFixed(2);
+  
+  if (x < 18.5) {
     bmiState = "overThin";
-  } else if (bmi < 24) {
+  } else if (18.5 <= x && x < 24) {
     bmiState = "normal";
-  } else if (bmi < 27) {
+  } else if (24 <= x && x < 27) {
     bmiState = "overWeight";
-  } else if (bmi < 30) {
+  } else if (27 <= x && x < 30) {
     bmiState = "mildFat";
-  } else if (bmi < 35) {
+  } else if (30 <= x && x < 35) {
     bmiState = "moderateFat";
-  } else if (bmi >= 35) {
+  } else if (x >= 35) {
     bmiState = "severeFat";
   } else {
     console.log("您的數值輸入錯誤，請重新輸入")
   }
-  return bmiState
-}
-function addRecord(bmi, bmiState) {
+  
   let obj = {};
   obj.bmi = bmi;
   obj.state = bmiState;
   bmiHistoryData.push(obj);
-}
-
-
-function printBmi(height, weight) {
   
-  const bmi = calculateBMI(weight, height)
-  const bmiState = getBmiState (bmi)
-
-  addRecord(bmi, bmiState)
-
   console.log(`您的體重${bmiStatesData[bmiState].state}，健康指數為${bmiStatesData[bmiState].color}`)
 }
 
 function showHistoryData(){
-  const totalRecord = bmiHistoryData.length;
-  const lastRecord = totalRecord - 1;
-  const lastState = bmiHistoryData[lastRecord].state
-  // console.log(totalRecord, lastRecord, lastState);
+  const totalNum = bmiHistoryData.length;
+  const lastNum = totalNum - 1;
+  const lastState = bmiHistoryData[lastNum].state
+  console.log(totalNum, lastNum, lastState);
 
-  console.log(`您總共計算 ${totalRecord} 次 BMI 紀錄，最後一次 BMI 指數為 ${bmiHistoryData[lastRecord].bmi}，體重${bmiStatesData[lastState].state}！健康指數為${bmiStatesData[lastState].color}！`);
+  console.log(`您總共計算 ${totalNum} 次 BMI 紀錄，最後一次 BMI 指數為 ${bmiHistoryData[lastNum].bmi}，體重${bmiStatesData[lastState].state}！健康指數為${bmiStatesData[lastState].color}！`);
 }
 
 // 以下需一行一行執行
 printBmi(178, 20); // 您的體重過輕，健康指數為藍色
 printBmi(178, 70); // 您的體重正常，健康指數為紅色
 printBmi(178, 85); // 您的體重過重，健康指數為澄色
-printBmi(200, 105)
 
 showHistoryData(); // 您總共計算 3 次 BMI 紀錄，最後一次 BMI 指數為 26.83，體重過重！健康指數為澄色！
